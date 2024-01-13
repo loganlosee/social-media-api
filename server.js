6a03e334a9ffc10879f81d9b20eadd64dd0d3ad9
Logan Losee <logan.losee99@gmail.com>
2024-01-13 12:24:41 -0700

    config and models init

--- server.js
+++ server.js
@@ -7,11 +7,6 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
-mongoose.connect('mongodb://localhost:27017/socialMediaDB', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
-
 // test db connection
 mongoose.connection.on('connected', () => {
   console.log('Connected to MongoDB database');
